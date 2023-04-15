b0c9f50cb9e6da80ba72c9aae1d7764ad637c493
dima.avdeev <dima.avdeev@jetbrains.com>
2023-04-15 21:43:19 +0400

    exact changes

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -29,10 +29,15 @@ android {
         versionName = "1.0"
     }
     compileOptions {
+        isCoreLibraryDesugaringEnabled = true
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
     kotlin {
         jvmToolchain(11)
     }
+}
+
+dependencies {
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.3")
 }
--- androidApp/src/androidMain/kotlin/com/myapplication/MainActivity.kt
+++ androidApp/src/androidMain/kotlin/com/myapplication/MainActivity.kt
@@ -4,11 +4,12 @@ import MainView
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import java.time.LocalDate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        println("LocalDate.now().month: ${LocalDate.now().month}")
         setContent {
             MainView()
         }
